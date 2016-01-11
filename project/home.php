b3596c7c5576891c4c0fb542373a683845fe3fbd
KirilKazakov <n0go0d@yahoo.com>
2016-01-11 06:02:54 +0200

    middle update

---
+++ project/home.php
@@ -0,0 +1,8 @@
+<?php
+    $page_title = 'home';
+    include('templates/header.php');
+?>
+
+<?php include('templates/posts.php') ?>
+
+<?php include('templates/footer.php') ?>
---
+++ project/templates/posts.php
@@ -0,0 +1,98 @@
+
+<section class="posts clearfix">
+	<div class="article-column clearfix">
+		<article>
+			<img src="img/posts/1.png" alt="">
+			<div class="text-holder">
+				<h2>Sticker Mule</h2>
+				<span>June 15, 2012  /  news, contests</span>
+				<p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores</p>
+				<button class="read-more">More</button>
+			</div>
+		</article>
+		<article>
+			<img src="img/posts/2.png" alt="">
+			<div class="text-holder">
+				<p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores</p>
+				
+			</div>
+
+		</article>
+		<article>
+			<img src="img/posts/3.png" alt="">
+			<div class="text-holder">
+				<p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores</p>
+				
+			</div>
+
+		</article>
+		
+	</div>
+	<div class="article-column">
+		<article>
+			<img src="img/posts/4.png" alt="">
+			<div class="text-holder">
+				<p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores</p>
+				
+			</div>
+
+		</article>
+		<article>
+		<img src="img/posts/5.png" alt="">
+		<div class="text-holder">
+				<p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores</p>
+				
+			</div>
+
+	</article>
+	<article>
+		<img src="img/posts/6.png" alt="">
+		<div class="text-holder">
+				<p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores</p>
+				
+			</div>
+
+	</article>
+	</div>
+	<div class="article-column">
+		<article>
+			<img src="img/posts/7.png" alt="">
+			<div class="text-holder">
+				<h2></h2>
+				<p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores</p>
+			</div>
+		</article>
+		<article>
+			<img src="img/posts/8.png" alt="">
+			<div class="text-holder">
+				<p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores</p>
+			</div>
+		</article>
+		<article>
+			<img src="img/posts/9.png" alt="">
+			<div class="text-holder">
+				<p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores</p>	
+			</div>
+		</article>
+	</div>	
+	<div class="article-column">
+		<article>
+			<img src="img/posts/10.png" alt="">
+			<div class="text-holder">
+				<p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores</p>
+			</div>			
+		</article>
+		<article>
+			<img src="img/posts/11.png" alt="">
+			<div class="text-holder">
+			<p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores</p>	
+			</div>			
+		</article>
+		<article>
+			<img src="img/posts/12.png" alt="">
+			<div class="text-holder">
+				<p>Lorem ipsum dolor sit amet, te possim inimicus ius. Alii ullam at corper pri ad, per nulla luptatum te, in qui delenit nostrum. Nam ad labores</p>
+			</div>			
+		</article>
+	</div>	
+</section>
